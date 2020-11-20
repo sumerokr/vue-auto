43162ef6b996b274db8fcb43716846db924c6f8a
Aleksandr Sadykov <sumerokr@gmail.com>
2020-11-20 23:01:35 +0100

    Update AppNav paddings

--- src/components/AppNav/AppNav.vue
+++ src/components/AppNav/AppNav.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="flex border-b border-solid border-indigo-500 px-4">
     <nav id="nav" class="flex">
-      <router-link class="p-2" to="/">Home</router-link>
-      <router-link class="p-2" to="/cars">Cars</router-link>
-      <router-link class="p-2" to="/service">Service</router-link>
+      <router-link class="p-3" to="/">Home</router-link>
+      <router-link class="p-3" to="/cars">Cars</router-link>
+      <router-link class="p-3" to="/service">Service</router-link>
     </nav>
-    <router-link class="ml-auto p-2" to="/user">User</router-link>
+    <router-link class="ml-auto p-3" to="/user">User</router-link>
   </div>
 </template>
 
