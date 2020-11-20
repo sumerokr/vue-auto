545e929db736e3b109375d530ec352978abfdfcc
Aleksandr Sadykov <sumerokr@gmail.com>
2020-11-20 20:03:36 +0100

    Fix slider

--- src/views/Cars.vue
+++ src/views/Cars.vue
@@ -10,17 +10,20 @@
           class="flex -mx-4 overflow-x-scroll space-x-2"
           style="scroll-snap-type: x mandatory; scroll-padding-left: 1rem"
         >
+          <li class="flex-shrink-0 w-2">&nbsp;</li>
           <li
-            class="flex-shrink-0 ml-4"
+            class="flex-shrink-0"
             style="scroll-snap-align: start; width: calc(100% - 2rem)"
           >
             <img
               class="max-w-full"
               src="https://fakeimg.pl/320x240/"
               srcset="
-                https://fakeimg.pl/320x240/   320w,
-                https://fakeimg.pl/640x480/   640w,
-                https://fakeimg.pl/1280x960/ 1280w
+                https://fakeimg.pl/320x240/    320w,
+                https://fakeimg.pl/640x480/    640w,
+                https://fakeimg.pl/960x720/    960w,
+                https://fakeimg.pl/1280x960/  1280w,
+                https://fakeimg.pl/1600x1180/ 1600w
               "
             />
           </li>
@@ -32,9 +35,11 @@
               class="max-w-full"
               src="https://fakeimg.pl/320x240/"
               srcset="
-                https://fakeimg.pl/320x240/   320w,
-                https://fakeimg.pl/640x480/   640w,
-                https://fakeimg.pl/1280x960/ 1280w
+                https://fakeimg.pl/320x240/    320w,
+                https://fakeimg.pl/640x480/    640w,
+                https://fakeimg.pl/960x720/    960w,
+                https://fakeimg.pl/1280x960/  1280w,
+                https://fakeimg.pl/1600x1180/ 1600w
               "
             />
           </li>
@@ -46,13 +51,15 @@
               class="max-w-full"
               src="https://fakeimg.pl/320x240/"
               srcset="
-                https://fakeimg.pl/320x240/   320w,
-                https://fakeimg.pl/640x480/   640w,
-                https://fakeimg.pl/1280x960/ 1280w
+                https://fakeimg.pl/320x240/    320w,
+                https://fakeimg.pl/640x480/    640w,
+                https://fakeimg.pl/960x720/    960w,
+                https://fakeimg.pl/1280x960/  1280w,
+                https://fakeimg.pl/1600x1180/ 1600w
               "
             />
           </li>
-          <li class="flex-shrink-0 w-4">&nbsp;</li>
+          <li class="flex-shrink-0 w-2">&nbsp;</li>
         </ul>
         <ul>
           <li></li>
