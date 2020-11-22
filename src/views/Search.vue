5a9bad20b689b2a09c509d21c665bbfb2f02112e
Aleksandr Sadykov <sumerokr@gmail.com>
2020-11-22 15:45:20 +0100

    Basic search

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,6 +1,112 @@
 <template>
   <div class="p-4">
     <h1 class="text-3xl mb-8">Search</h1>
+
+    <form @submit.prevent="onSearch">
+      <label for="brand">Brand</label><br />
+      <input
+        class="border border-gray-200 p-2 w-full"
+        type="text"
+        id="brand"
+      /><br /><br />
+
+      <label for="model">Model</label><br />
+      <input
+        class="border border-gray-200 p-2 w-full"
+        type="text"
+        id="model"
+      /><br /><br />
+
+      <div class="flex space-x-4">
+        <div class="flex-1">
+          <label for="model">Min year</label><br />
+          <select
+            class="border border-gray-200 p-2 w-full"
+            type="text"
+            id="model"
+          >
+            <option
+              v-for="year in new Array(20)
+                .fill(null)
+                .map((item, index) => 2020 - index)"
+              :value="year"
+              :key="year"
+            >
+              {{ year }}
+            </option>
+          </select>
+        </div>
+        <div class="flex-1">
+          <label for="model">Max year</label><br />
+          <select
+            class="border border-gray-200 p-2 w-full"
+            type="text"
+            id="model"
+          >
+            <option
+              v-for="year in new Array(20)
+                .fill(null)
+                .map((item, index) => 2020 - index)"
+              :value="year"
+              :key="year"
+            >
+              {{ year }}
+            </option>
+          </select>
+        </div>
+      </div>
+
+      <br />
+
+      <div class="flex space-x-4">
+        <div class="flex-1">
+          <label for="model">Min mileage</label><br />
+          <input
+            class="border border-gray-200 p-2 w-full"
+            type="text"
+            id="model"
+          />
+        </div>
+        <div class="flex-1">
+          <label for="model">Max mileage</label><br />
+          <input
+            class="border border-gray-200 p-2 w-full"
+            type="text"
+            id="model"
+          />
+        </div>
+      </div>
+
+      <br />
+
+      <div class="flex space-x-4">
+        <div class="flex-1">
+          <label for="model">Min prce</label><br />
+          <input
+            class="border border-gray-200 p-2 w-full"
+            type="text"
+            id="model"
+          />
+        </div>
+        <div class="flex-1">
+          <label for="model">Max price</label><br />
+          <input
+            class="border border-gray-200 p-2 w-full"
+            type="text"
+            id="model"
+          />
+        </div>
+      </div>
+
+      <br />
+
+      <button
+        class="bg-blue-500 text-white px-4 py-3 rounded text-opacity-90"
+        type="submit"
+      >
+        Search
+      </button>
+    </form>
   </div>
 </template>
 
@@ -9,5 +115,13 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "Search",
+
+  setup: () => {
+    return {
+      onSearch: () => {
+        alert("Search attempt");
+      },
+    };
+  },
 });
 </script>
