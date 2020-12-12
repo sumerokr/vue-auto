da225ce18e2624a04244d76c4589afdfdd1bc647
Aleksandr Sadykov <sumerokr@gmail.com>
2020-12-12 10:47:32 +0100

    use msw for all envs

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ import store from "./store";
 import "./assets/tailwind.css";
 import { worker } from "./mocks/browser";
 
-if (process.env.NODE_ENV === "development") {
-  worker.start();
-}
+// if (process.env.NODE_ENV === "development") {
+worker.start();
+// }
 
 createApp(App).use(store).use(router).mount("#app");
