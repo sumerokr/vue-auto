b9dd61650f93655e1aa808cda34d99f8e6c57c9c
Aleksandr Sadykov <sumerokr@gmail.com>
2020-12-24 14:31:17 +0100

    Remove tap-highlight for IconButton

--- src/components/IconButton/IconButton.vue
+++ src/components/IconButton/IconButton.vue
@@ -1,6 +1,7 @@
 <template>
   <button
     class="flex items-start p-3 bg-black bg-opacity-5 focus:bg-opacity-20 focus:outline-none rounded"
+    style="tap-highlight-color: rgba(255, 255, 255, 0)"
   >
     <slot></slot>
   </button>
